01a7a8282f4fdd0b5f0e66f52facb565f14b43a5
Yukihiro Nakadaira <yukihiro.nakadaira@gmail.com>
2011-03-05 22:59:18 +0900

    Fixed for ruby 1.9

--- src/main.rb
+++ src/main.rb
@@ -3,8 +3,13 @@ require "optparse"
 require "dl/import"
 
 module VimRemote
-  extend DL::Importable
-  dlload File.join(File.dirname(File.expand_path(__FILE__)), "vimremote.so")
+  if RUBY_VERSION >= '1.9.0'
+    extend DL::Importer
+  else
+    extend DL::Importable
+  end
+  dlload File.join(File.dirname(File.expand_path(__FILE__)),
+      RUBY_PLATFORM.downcase.include?("mswin") ? "vimremote.dll" : "vimremote.so")
   typealias("size_t", "unsigned long")
   extern "void* vimremote_malloc(size_t)"
   extern "void vimremote_free(void*)"
@@ -17,7 +22,11 @@ module VimRemote
 end
 
 def remote_expr(servername, expr)
-  result = DL::PtrData.new(0)
+  if RUBY_VERSION >= '1.9.0'
+    result = DL::CPtr.new(0)
+  else
+    result = DL::PtrData.new(0)
+  end
   result.free = VimRemote.extern('void vimremote_free(void*)')
   if VimRemote.vimremote_remoteexpr(servername, expr, result.ref) != 0
     raise "vimremote_remoteexpr() failed: #{result.to_s}"
@@ -26,7 +35,11 @@ def remote_expr(servername, expr)
 end
 
 def command_serverlist()
-  servernames = DL::PtrData.new(0)
+  if RUBY_VERSION >= '1.9.0'
+    servernames = DL::CPtr.new(0)
+  else
+    servernames = DL::PtrData.new(0)
+  end
   servernames.free = VimRemote.extern('void vimremote_free(void*)')
   if VimRemote.vimremote_serverlist(servernames.ref) != 0
     raise "vimremote_serverlist() failed"
@@ -39,19 +52,35 @@ def command_remoteexpr(servername, expr)
 end
 
 def command_server(servername)
-  feval = DL.callback('ISP') {|expr, result|
-    begin
-      res = eval(expr).to_s
-      err = 0
-    rescue => exc
-      res = exc.to_s
-      err = -1
-    end
-    result.struct!('S', 'val')
-    result['val'] = VimRemote.vimremote_malloc(res.size + 1)
-    result['val'][0, res.size + 1] = res
-    err
-  }
+  if RUBY_VERSION >= '1.9.0'
+    feval = VimRemote.bind('int eval(char*, char**)') {|expr, result|
+      begin
+        res = eval(expr.to_s).to_s
+        err = 0
+      rescue => exc
+        res = exc.to_s
+        err = -1
+      end
+      m = VimRemote.vimremote_malloc(res.size + 1)
+      m[0, res.size + 1] = res
+      result[0, DL::SIZEOF_VOIDP] = m.ref[0, DL::SIZEOF_VOIDP]
+      err
+    }
+  else
+    feval = DL.callback('ISP') {|expr, result|
+      begin
+        res = eval(expr).to_s
+        err = 0
+      rescue => exc
+        res = exc.to_s
+        err = -1
+      end
+      result.struct!('S', 'val')
+      result['val'] = VimRemote.vimremote_malloc(res.size + 1)
+      result['val'][0, res.size + 1] = res
+      err
+    }
+  end
   if VimRemote.vimremote_register(servername, feval) != 0
     raise "vimremote_register() failed"
   end
